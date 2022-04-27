8a2ab7d04f038945e6ee552ebc6ce4d961278d95
elisedesbarats <74312046+elisedesbarats@users.noreply.github.com>
2022-04-27 12:47:05 -0400

    figured out the graph a little bit

--- analysis.py
+++ analysis.py
@@ -4,8 +4,8 @@
 import neurokit2 as nk
 import pandas as pd
 import bioread
-import  matplotlib.pyplot as plt
-
+import matplotlib
+import matplotlib.pyplot as plt
 
 '''
 Import the Data 
@@ -16,14 +16,23 @@ plt.rcParams['figure.figsize'] = [15, 9]  # Bigger images
 plt.rcParams['font.size']= 13
 
 filename = "/Users/elise/PycharmProjects/MASMRanalysis/MASMRpractice/003ASMR.acq"
-eda_signal, sampling_rate = nk.read_acqknowledge(filename)
+data, sampling_rate = nk.read_acqknowledge(filename)
 
 
 '''
  Process the raw EDA signal (unfinished)
 '''
 
-signal, info = nk.eda_process(eda_signal["EDA, Y, PPGED-R"], sampling_rate=2000)
+signal, info = nk.eda_process(data["EDA, Y, PPGED-R"], sampling_rate=2000) #cleans the signal
+
+# Extract clean EDA and SCR features
+cleaned = signal["EDA_Clean"]
+features = [info["SCR_Onsets"], info["SCR_Peaks"], info["SCR_Recovery"]]
+
+# Visualize
+
+plot = nk.eda_plot(signal)
+plt.show()
 
 '''
 FIND EVENTS 
@@ -32,12 +41,6 @@ label them with a time and a name for which type of stimulus it was
 '''
 
 events = {}
-
-
-# Process ecg
-#ecg_signals, info = nk.ecg_process(data["ECG"], sampling_rate=2000)
-#plot = nk.ecg_plot(ecg_signals[:3000], sampling_rate=100)
-
 
 #UNFINISHED - create a dictionary of events and pass events into the events parameter
 #epochs = nk.epochs_create(ecg_signals, events=[0, 15000], sampling_rate=100, epochs_start=0, epochs_end=150)
